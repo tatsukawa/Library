f730adebff364b6727f49af775db590033194cfe
Tsuyoshi Tatsukawa <ts.tatsukawa@gmail.com>
2016-11-06 07:12:19 +0900

    [util] update template

--- util/template.cpp
+++ util/template.cpp
@@ -3,6 +3,12 @@
 #define get_variable_name(x) #x
 #define debug(x) cout << #x << " = " << x << endl
 
+#define ALL(x) (x).begin(), (x).end()
+#define SORT(x) sort((x).begin(), (x).end())
+#define REVERSE(x) reverse( (x).begin(),(x).end())
+#define UNIQUE(x) (x).erase(unique(ALL((x))), (x).end())
+#define PW(x) (1LL<<(x))
+
 using namespace std;
 using ll = int64_t;
 using P = pair<int, int>;
@@ -16,9 +22,8 @@ ostream& operator<<(ostream &os, const pair<T, U> &p) {
 template<class T>
 ostream& operator <<(ostream& os, const vector<T> &v){
     cout << get_variable_name(v) << " = [";
-    for(auto i : v) {
+    for(auto i : v) 
         os << " " << v[i];
-    }
     os << " ]";
     return os;
 }
